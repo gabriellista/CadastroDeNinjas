82161464501ee07bcbe41b07d5be7e17c4d99851
Gabriel Felipe <gabrielflds@gmail.com>
2026-07-09 13:56:54 -0300

    :feat:Criar rotas do Controller com mock

---
+++ src/main/java/com/gabrielLista/CadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,35 @@
+package com.gabrielLista.CadastroDeNinjas.Missoes;
+
+
+import jakarta.persistence.PreUpdate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+
+public class MissoesController {
+
+    // GET--Mandar uma requisao para mostrar as missoes
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return"Missãoes listadasc om sucesso";
+    }
+    // POST--Mandar uma requisao para criar as missoes
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão criada com sucesso";
+    }
+
+    // PUT--Mandar uma requisao para alterar as missoes
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missão alterada com sucesso";
+    }
+
+    // DEL--Mandar uma requisao para deletar as missoes
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missão deletada com sucesso";
+    }
+
+}
