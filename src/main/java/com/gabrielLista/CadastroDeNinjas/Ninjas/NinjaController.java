3e2ec7ad648057293542f67f752bda2bd61690b9
Gabriel Felipe <gabrielflds@gmail.com>
2026-07-09 13:45:58 -0300

    feate:Atualiza o controller com o mock de dados

--- src/main/java/com/gabrielLista/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/gabrielLista/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package com.gabrielLista.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,5 +10,31 @@ public class NinjaController {
     public String boasVindas(){
         return "Essa é a minha primeira mensagem";
     }
+    //ADICIONAR NINJA
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja criado";
+    }
+    //MOSTRAR TODOS OS NINJAS
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Mostrar Ninja";
+    }
+    //MOSTRAR NINJA POR ID
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasporId() {
+        return "Mostrar Ninja por ID";
+    }
 
+    //ALTERAR NINJA
+        @PutMapping("/alterarID")
+        public String alterarNinjasporId() {
+            return "Alterar Ninja por ID";
+        }
+
+        //DELETAR NINJA
+        @DeleteMapping("/deletarID")
+        public String deletarNinjaPorId() {
+            return "deletar";
+        }
 }
