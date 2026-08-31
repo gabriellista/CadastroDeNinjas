eb79a5bd31eeb9239aed45cfd1ca96172a0c0dce
Gabriel Felipe <gabrielflds@gmail.com>
2026-08-31 14:56:00 -0300

    feat: adiciona listagem e edicao de missoes com thymeleaf

---
+++ src/main/java/com/gabrielLista/CadastroDeNinjas/Missoes/MissoesViewController.java
@@ -0,0 +1,40 @@
+package com.gabrielLista.CadastroDeNinjas.Missoes;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/web/missoes")
+
+public class MissoesViewController {
+
+    private final MissoesService missoesService;
+
+    public MissoesViewController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
+    @GetMapping
+    public String listarMissoes(Model model){
+        List<MissoesDTO> missoes = missoesService.listarMissoes();
+        model.addAttribute("missoes", missoes);
+        return "missoes/lista";
+    }
+
+    @GetMapping("/{id}/editar")
+    public String exibirFormularioEdicao(@PathVariable Long id,Model model){
+        MissoesDTO missao = missoesService.buscarMissaoPorId(id);
+        model.addAttribute("missao",missao);
+        return "missoes/editar";
+    }
+
+    @PostMapping("/{id}/editar")
+    public String salvarEdicao(@PathVariable Long id, @ModelAttribute MissoesDTO missoesDTO){
+        MissoesDTO missao = missoesService.atualizarMissao(id, missoesDTO);
+        return "redirect:/web/missoes";
+    }
+}
